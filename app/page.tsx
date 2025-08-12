b154fc389f98265f26068d8be7f203dbaf30c9bd
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-08-12 16:37:34 +0200

    globals.css updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col">
       <h1>Bienvenu dans Kratos <span>.System</span></h1>      
-      <button className="btn">Se connecter</button>
+      <button className="btn btn-accent">Se connecter</button>
       <button>Créer Mon compte</button>
     </div>
   );
