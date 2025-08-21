f785916eec65470ad707fe83c617818ef89e95a8
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-08-21 11:48:44 +0200

    LogoSystem.tsx initialised

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <h1>Bienvenu dans Kratos <span>.System</span></h1>      
-      <div className="flex flex-col gap-4">
-        <button className="btn">Se connecter</button>
-        <button>Créer Mon compte</button>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold mb-4">Bienvenu dans Kratos <span>.System</span></h1>
+        <div className="flex flex-col gap-4">
+          <button className="btn btn-accent">Se connecter</button>
+          <button className="btn">Créer Mon compte</button>
+        </div>
       </div>
     </div>
   );
