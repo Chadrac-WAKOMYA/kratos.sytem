0b163c40e047cd758d7020eab6609d10dddb1f37
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-08-21 10:54:27 +0200

    NavBar initialised

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col">
       <h1>Bienvenu dans Kratos <span>.System</span></h1>      
-      <div>
+      <div className="flex flex-col gap-4">
         <button className="btn">Se connecter</button>
         <button>Créer Mon compte</button>
       </div>
