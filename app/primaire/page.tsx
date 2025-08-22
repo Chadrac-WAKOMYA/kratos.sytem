719f4b3051b080089260cb5f239c538c28b8cdff
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-08-22 21:28:03 +0200

    primaire home page inirialised$

--- app/primaire/page.tsx
+++ app/primaire/page.tsx
@@ -6,7 +6,12 @@ type Props = {}
 export default function page({}: Props) {
   return (
     <Wrapper>
-      <div className='flex items-center justify-center min-h-screen'>page</div>
+      {/* <div className='flex items-center justify-center min-h-screen'>page</div> */}
+      <div className='flex justify-between'>
+        <h1>Primaire</h1>
+        <input type="text" />
+      </div>
+      <div></div>
     </Wrapper>
   )
 }
