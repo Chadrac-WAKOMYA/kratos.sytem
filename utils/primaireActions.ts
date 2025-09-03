2710b777a3cd987550a345fa61e97d3be3850f86
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-09-03 06:02:28 +0200

    primaireActions.ts finished

--- utils/primaireActions.ts
+++ utils/primaireActions.ts
@@ -1,1 +1,13 @@
 import connectionDB from "./connectionDB";
+export async function getAnneeScolaires() {
+    try {
+        const anneeScolaires = await connectionDB.AnneeScolaire.findMany();
+        if (!anneeScolaires) {
+            throw new Error("Annees Scolaires non trouvées");
+        }
+        
+        return anneeScolaires;
+    } catch (error) {
+        console.error("Error fetching AnneeScolaire", error);
+    }
+}
