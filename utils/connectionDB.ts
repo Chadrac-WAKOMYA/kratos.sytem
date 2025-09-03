7feb312e4c35afa7220d7e96899ddcf37b3fe6f7
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-09-03 05:52:02 +0200

    primaireActions.ts initialised

--- utils/connectionDB.ts
+++ utils/connectionDB.ts
@@ -8,8 +8,8 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const connectionDB = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-export default prisma
+export default connectionDB
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = connectionDB
---
+++ utils/primaireActions.ts
@@ -0,0 +1,1 @@
+import connectionDB from "./connectionDB";
