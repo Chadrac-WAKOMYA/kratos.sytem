c822d199cb55489d2ada643e6f9995cb723589f6
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-09-18 05:30:57 +0200

    Année Scolaire select retouched

--- app/primaire/page.tsx
+++ app/primaire/page.tsx
@@ -8,7 +8,6 @@ export default function page({ }: Props) {
     <Wrapper>
       <div>
         <div className='flex justify-end '>
-          {/* <div ><span className='text-gray-700 md:text-2xl'>Pimaire</span></div> */}
           <div className='flex items-center'>
             <label htmlFor="options" className="text-gray-700 mr-2">
               Année Scolaire :
@@ -18,9 +17,9 @@ export default function page({ }: Props) {
               className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500"
             >
               <option value="">Sélectionnez...</option>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
+              <option value="option1">2025 - 2026</option>
+              <option value="option2">2024 - 2025</option>
+              <option value="option3">2023 - 2024</option>
             </select>
           </div>
         </div>
