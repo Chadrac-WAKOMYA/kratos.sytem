832569240b96f3ff2f1f0f338ef7cca36fd1cbf9
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-10-05 21:17:31 +0200

    confirmation updated

--- app/primaire/confirmation/page.tsx
+++ app/primaire/confirmation/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 type Props = {}
 
-// export default function Confirmation({}: Props) {
+export default function Confirmation({}: Props) {
   return (
-    // <div>Confirmation</div>
+    <div>Confirmation</div>
   )
 }
