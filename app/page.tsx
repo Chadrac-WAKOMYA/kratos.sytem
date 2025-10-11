d3a75588d3d13b368abd4061b4dcd8d7439bc36a
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-10-11 21:48:59 +0200

    home page updated again

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <h1 className="mt-6">Bienvenu dans le sytème du complexe scolaire Kratos </h1>
         {/* <h2 className="mb-2">Un système adapté aux besoins scolaires </h2> */}
         <h1 className="mb-2">Efficace et intuitif </h1>
-        <div className="flex gap-4 mt-6 mb-2">
+        {/* <div className="flex gap-4 mt-6 mb-2"> */}
           <Link className="btn btn-sm btn-accent " href={"/primaire"}>Se connecter</Link>
           <button className="btn ">Créer Mon compte</button>
         </div>
