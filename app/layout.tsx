fb0903e16d563c74694e3ad11040cd0f0ac5ac76
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-11-07 12:56:33 +0200

    ready to continue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  // subsets: ["latin"],
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
