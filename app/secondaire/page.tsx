78f40b2780e3ffca1d5c0467fe08db99de7f0acb
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-11-08 21:17:36 +0200

    secondaire module retouched

--- app/secondaire/page.tsx
+++ app/secondaire/page.tsx
@@ -1,12 +1,11 @@
 import Wrapper from '@/component/Wrapper'
-import React from 'react'
 
 type Props = {}
 
 export default function page({}: Props) {
   return (
       <Wrapper>
-        <div className='flex items-center justify-center min-h-screen'>Secondaire</div>
+        {/* <div className='flex items-center justify-center min-h-screen'>Secondaire</div> */}
       </Wrapper>
     )
 }
