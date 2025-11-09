87d9d0e3c4a7d42948c612a355ac24c0aed45238
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-11-09 22:05:59 +0200

    secondaire module retouched

--- app/maternelle/page.tsx
+++ app/maternelle/page.tsx
@@ -5,7 +5,7 @@ type Props = {}
 export default function page({}: Props) {
   return (
       <Wrapper>
-        {/* <div className='flex items-center justify-center min-h-screen'>Maternelle</div> */}
+        <div className='flex items-center justify-center min-h-screen'>Maternelle</div>
       </Wrapper>
     )
 }
