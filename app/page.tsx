78489eba60fee8f6b5d9eedf669946ab7d71d583
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-11-15 17:28:40 +0200

    home page retouched again

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       {/* <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg"> */}
         <LogoSystemV />
-        {/* <h1 className="mt-6">Bienvenu dans le sytème du complexe scolaire Kratos </h1> */}
+        <h1 className="mt-6">Bienvenu dans le sytème du complexe scolaire Kratos </h1>
         <h2 className="mb-2">Un système adapté aux besoins scolaires </h2>
         <h1 className="mb-2">Efficace et intuitif </h1>
         <div className="flex gap-4 mt-6 mb-2">
