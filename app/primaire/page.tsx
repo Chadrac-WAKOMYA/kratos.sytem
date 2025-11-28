8e3b1a96c17b9af540d1c5d3f246cceb834e0779
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-11-28 21:11:10 +0200

    MATERNELLE home page updated

--- app/primaire/page.tsx
+++ app/primaire/page.tsx
@@ -8,7 +8,7 @@ export default function page({ }: Props) {
       <div>
         <div>test</div>
         <div className='flex justify-end '>
-          <div className='flex items-center'>
+          {/* <div className='flex items-center'> */}
             <label htmlFor="options" className="text-gray-700 mr-2">
               <label htmlFor="options" className="text-gray-700 mr-2"></label>
               Année Scolaire :
