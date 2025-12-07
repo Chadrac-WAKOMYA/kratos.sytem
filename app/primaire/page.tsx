9c7e0ce27d6da47845275fc5061d44921fcca859
Chadrac-WAKOMYA <chadracxx@gmail.com>
2025-12-07 21:58:48 +0200

    READY TO CONTINUE

--- app/primaire/page.tsx
+++ app/primaire/page.tsx
@@ -10,7 +10,7 @@ export default function page({ }: Props) {
         {/* <div className='flex justify-end '> */}
           {/* <div className='flex items-center'> */}
             <label htmlFor="options" className="text-gray-700 mr-2">
-              <label htmlFor="options" className="text-gray-700 mr-2"></label>
+              {/* <label htmlFor="options" className="text-gray-700 mr-2"></label> */}
               Année Scolaire :
             </label>
             <select
